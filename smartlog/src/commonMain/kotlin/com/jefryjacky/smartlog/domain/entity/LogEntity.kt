0db295e4975fa2344eea822b723ed5f7b5d3b29c
Jefry Jacky <jefryjacky@gmail.com>
2025-07-16 17:18:09 +0700

    save timezone

--- smartlog/src/commonMain/kotlin/com/jefryjacky/smartlog/domain/entity/LogEntity.kt
+++ smartlog/src/commonMain/kotlin/com/jefryjacky/smartlog/domain/entity/LogEntity.kt
@@ -1,9 +1,11 @@
 package com.jefryjacky.smartlog.domain.entity
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
 
 data class LogEntity(
     val date: LocalDateTime,
+    val timezone: TimeZone,
     val tag: String,
     val message: String,
     val error: String? = null
