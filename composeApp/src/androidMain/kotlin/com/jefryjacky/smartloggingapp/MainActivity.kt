4767a7f344c09765a60983c62714c31cfc124c58
Jefry Jacky <jefryjacky@gmail.com>
2025-07-22 20:34:55 +0700

    running log non stop

--- composeApp/src/androidMain/kotlin/com/jefryjacky/smartloggingapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jefryjacky/smartloggingapp/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import com.jefryjacky.smartlog.LogActivity
 import com.jefryjacky.smartlog.LogCatPrinter
 import com.jefryjacky.smartlog.SmartLog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +22,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             App{
+                runningLog()
                 val intent = Intent(this, LogActivity::class.java)
                 startActivity(intent)
+            }
+        }
+    }
+
+    private fun runningLog(){
+        lifecycleScope.launch(Dispatchers.IO) {
+            var count = 0
+            while (true) {
+                SmartLog.v("LogViewModel", "test message$count")
+                delay(5000)
+                SmartLog.d("LogViewModel", "test message$count")
+                delay(5000)
+                SmartLog.i("LogViewModel", "test message$count")
+                delay(5000)
+                SmartLog.w("LogViewModel", "test message$count")
+                delay(5000)
+                SmartLog.e("LogViewModel", "test message$count")
+                delay(5000)
+                SmartLog.wtf(
+                    "${count }LogViewModel",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                )
+                ++count
             }
         }
     }
--- smartlog/src/commonMain/kotlin/com/jefryjacky/smartlog/ui/logs/LogViewModel.kt
+++ smartlog/src/commonMain/kotlin/com/jefryjacky/smartlog/ui/logs/LogViewModel.kt
@@ -22,22 +22,7 @@ class LogViewModel(
 
 
     init {
-
             getLog()
-
-        viewModelScope.launch {
-            SmartLog.v("LogViewModel", "test message1")
-            delay(5000)
-            SmartLog.d("LogViewModel", "test message2")
-            delay(5000)
-            SmartLog.i("LogViewModel", "test message3")
-            delay(5000)
-            SmartLog.w("LogViewModel", "test message4")
-            delay(5000)
-            SmartLog.e("LogViewModel", "test message5")
-            delay(5000)
-            SmartLog.wtf("LogViewModel", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        }
     }
     val state = _state.asStateFlow()
     private val _filterBottomSheet = MutableStateFlow(FilterBottomState())
